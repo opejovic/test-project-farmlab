e86dc657b4b9449ffacfc26e6c03340d5611f7e3
Ognjen Pejovic <opejovic@gmail.com>
2018-08-11 17:23:29 +0200

    Add HomeController and models -mc Practice, LabResult, File (Also edit the migrations for the models)

---
+++ database/migrations/2018_08_11_145939_create_practices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePracticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('practices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->integer('vet_id'); // has many vets
+            $table->integer('result_id'); // has many results
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('practices');
+    }
+}
---
+++ database/migrations/2018_08_11_150357_create_lab_results_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLabResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lab_results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('herd_number');
+            $table->date('date_of_arrival');
+            $table->date('date_of_test');
+            $table->string('animal_id');
+            $table->string('lab_code');
+            $table->string('type_of_samples');
+            $table->string('reading');
+            $table->string('interpretation');
+            $table->string('farmer_name');
+            $table->enum('status', ['processed', 'unprocessed']);
+            $table->text('vet_comment');
+            $table->string('vet_indicator');
+            $table->integer('practice_id');
+            $table->integer('vet_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lab_results');
+    }
+}
