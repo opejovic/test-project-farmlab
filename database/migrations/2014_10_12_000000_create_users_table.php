dc767bba98f2c5184d5002dd658b889987f9e868
Ognjen Pejovic <opejovic@gmail.com>
2018-08-11 16:59:21 +0200

    Add required fields to create_users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('type', ['ADMIN', 'FARM_LAB_TEAM_MEMBER', 'PRACTICE_ADMIN', 'PRACTICE_VET']);
+            $table->string('one_singal_id')->nullable(); // only vets will have these
+            $table->string('practice_id')->nullable(); // farmlab and admin dont have practice
+            $table->string('status'); // pitati paku
             $table->rememberToken();
             $table->timestamps();
         });
