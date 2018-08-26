f0d06a80a8e82cece246f66aa6698efd45efad3b
Ognjen Pejovic <opejovic@gmail.com>
2018-08-26 08:28:26 +0200

    Edit notes and comment out some routes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ Route::get('/login', 'HomeController@create')->name('login');
 Route::post('/login', 'HomeController@store');
 Route::get('/logout', 'HomeController@destroy');
 
-Route::get('/farmlab', 'PracticeController@index');
+// Route::get('/farmlab', 'PracticeController@index');
 Route::get('/farmlab/create', 'PracticeController@create');
 Route::post('/farmlab/create/user', 'PracticeController@store');
 
-Route::get('/practice', 'VetController@index');
+// Route::get('/practice', 'VetController@index');
 Route::get('/practice/create/vet', 'VetController@create');
 Route::post('/practice/create/vet', 'VetController@store');
 
