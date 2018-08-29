58c232021f1c43c0b8116d34db27d70fa965cd3f
Ognjen Pejovic <opejovic@gmail.com>
2018-08-29 20:25:51 +0200

    Small Refactor

--- app/File.php
+++ app/File.php
@@ -11,22 +11,20 @@ class File extends Model
 {
 	protected $fillable = ['name', 'file_path'];
 
-	//check to see if the requested file matches other uploaded files by name
-
     public function upload()
     {
-        $file = request()->file('csv_file');
+        $file = request('csv_file');
     	$fileName = $file->getClientOriginalName();
 
     	if (! Storage::exists("labresults/{$fileName}")) {
 	        $filePath = Storage::putFileAs('labresults', $file, $fileName);
 
 	        $this->create([
-	            'name' => request('csv_file')->getClientOriginalName(),
+	            'name' => $fileName,
 	            'file_path' => storage_path($filePath)
 	        ]); 
             
-            LabResult::parseAndSave();
+            LabResult::parseAndSave($file);
 
     	} else {
     		
