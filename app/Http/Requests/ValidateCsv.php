d8806047fb0da5de9e5f0a7c789d792e95fbfe5d
Ognjen Pejovic <opejovic@gmail.com>
2018-08-29 20:29:08 +0200

    Refactor

--- app/Http/Requests/ValidateCsv.php
+++ app/Http/Requests/ValidateCsv.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use App\File;
-use App\Jobs\ParseAndInsert;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ValidateCsv extends FormRequest
@@ -31,26 +30,40 @@ class ValidateCsv extends FormRequest
         ];
     }
 
-
     /**
-     * Check if the header columns are in the correct order
+     * Check if the header columns in the requested file 
+     * exist and are in following order
      *
-     * 
+     * @return bool
      */
     public function checkHeader()
         {
-            $upload = request()->file('csv_file');
-            $getPath = $upload->getRealPath();
+            $file = request()->file('csv_file');
+            $getPath = $file->getRealPath();
             $csv_file = fopen($getPath, 'r');
             $header = fgetcsv($csv_file, 0, ',');
 
             $countheader = count($header); 
 
-            if ($countheader < 14 && in_array('herd_number', $header) && in_array('date_of_arrival', $header) && in_array('date_of_test', $header) && in_array('animal_id', $header) && in_array('lab_code', $header) && in_array('test_name', $header) && in_array('type_of_samples', $header) && in_array('reading', $header) && in_array('interpretation', $header) && in_array('farmer_name', $header) && in_array('vet_comment', $header) && in_array('vet_indicator', $header) && in_array('practice_id', $header)) { 
+            if ($countheader < 14 
+                    && in_array('herd_number', $header) 
+                    && in_array('date_of_arrival', $header) 
+                    && in_array('date_of_test', $header) 
+                    && in_array('animal_id', $header) 
+                    && in_array('lab_code', $header) 
+                    && in_array('test_name', $header) 
+                    && in_array('type_of_samples', $header) 
+                    && in_array('reading', $header) 
+                    && in_array('interpretation', $header) 
+                    && in_array('farmer_name', $header) 
+                    && in_array('vet_comment', $header) 
+                    && in_array('vet_indicator', $header) 
+                    && in_array('practice_id', $header)) { 
 
-                    return true;
-                    fclose($csv_file);
+                        return true;
+                        fclose($csv_file);
                 }
+
             fclose($csv_file);           
         }
 }
