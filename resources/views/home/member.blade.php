7fab71b43f37e26d552a6917910ae3923128a8e2
Ognjen Pejovic <opejovic@gmail.com>
2018-09-01 14:48:54 +0200

    Add design components

--- resources/views/home/member.blade.php
+++ resources/views/home/member.blade.php
@@ -3,22 +3,36 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Hello, {{ \Auth::user()->name }}</div>
-
                       <div class="card-body">
                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                       </div>
+                </div>
+          </div>
 
-                      <div class="card-body">
-                            <a href="/farmlab/create" class="btn btn-md btn-secondary">Create practice</a>
-                            <a href="/file/upload" class="btn btn-md btn-secondary">Upload result</a>
-                            {{-- <a href="/labresults/process" class="btn btn-md btn-secondary">Process the result</a> --}}
-                      </div>
-
+        <div class="col-md-4">
+          <div class="card">
+                <div class="card-header text-center">Dashboard</div>
+                    <div class="card-body text-center">
+                        <a href="/farmlab/create" class="btn btn-md btn-secondary">Create practice</a>
+                           <hr>
+                        <a href="/file/upload" class="btn btn-md btn-secondary">Upload result</a>
+                    </div>
+                </div>
+<br>
+          <div class="card">
+                <div class="card-header text-center">Your activity</div>
+                    <div class="card-body text-center">
+                        Lorem ipsum.
+                    </div>
+                </div>
             </div>
+
+
         </div>
     </div>
-</div>
+
 @endsection
--- resources/views/home/practice.blade.php
+++ resources/views/home/practice.blade.php
@@ -10,9 +10,18 @@
                     <div class="card-body">
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </div>
+                    
+                </div>
+            </div>
 
-                    <div class="card-body">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header text-center">Dashboard</div>
+
+
+                    <div class="card-body text-center">
                         <a href="/practice/create/vet" class="btn btn-md btn-secondary">Add new vet</a>
+                        <hr>
                         <a href="/labresults/index" class="btn btn-md btn-secondary">See results</a>
                     </div>
                     
