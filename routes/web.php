9b2ecc04a68d04e988df76367b9f3de980896360
Ognjen Pejovic <opejovic@gmail.com>
2018-09-11 23:50:57 +0200

    Refactor

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,32 @@
 <?php
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index');
-Route::get('/login', 'HomeController@create')->name('login');
-Route::post('/login', 'HomeController@store');
-Route::get('/logout', 'HomeController@destroy')->name('logout');
+Route::get('/login', 'HomeController@create')->middleware('guest');
+Route::post('/login', 'HomeController@store')->name('login')->middleware('guest');
 
-Route::get('/farmlab/create', 'PracticeController@create')->name('create');
-Route::post('/farmlab/create', 'PracticeController@store');
 
-Route::get('/practice/create/vet', 'VetController@create')->name('vet.create');
-Route::post('/vet', 'VetController@store')->name('vet.store');
+Route::group(['middleware' => ['auth']], function ()
+{
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/home', 'HomeController@index');
+    Route::get('/logout', 'HomeController@destroy')->name('logout');
 
-Route::get('/file/upload', 'FileController@create')->name('file.create');
-Route::post('/file', 'FileController@store')->name('file.store');
+    Route::get('/farmlab/create', 'PracticeController@create')->name('create');
+    Route::post('/farmlab/create', 'PracticeController@store');
 
-Route::get('/labresults/index', 'LabResultController@index')->name('labresults.index');
-Route::get('/labresults/{result}', 'LabResultController@show')->name('labresults.show');
-Route::post('/labresults/{result}', 'LabResultController@edit')->name('labresults.process');
+    Route::get('/practice/create/vet', 'VetController@create')->name('vet.create');
+    Route::post('/vet', 'VetController@store')->name('vet.store');
+
+    Route::get('/file/upload', 'FileController@create')->name('file.create');
+    Route::post('/file', 'FileController@store')->name('file.store');
+
+    Route::get('/labresults/index', 'LabResultController@index')->name('labresults.index')->middleware('practice');
+    Route::get('/labresults/farmer/{farmer}', 'LabResultController@index')->name('labresults.farmer');
+    Route::get('/labresults/{result}', 'LabResultController@show')->name('labresults.show');
+    Route::post('/labresults/{result}', 'LabResultController@update')->name('labresults.process');
+
+});
+
+
 
 
 
