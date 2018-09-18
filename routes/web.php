5c787be7dae31323ffa16102da7c0aa645ab2661
Ognjen Pejovic <opejovic@gmail.com>
2018-09-18 21:56:25 +0200

    Refactor and remove middleware from home.index route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
 Route::get('/login', 'HomeController@create')->middleware('guest');
 Route::post('/login', 'HomeController@store')->name('login')->middleware('guest');
 
 
 Route::group(['middleware' => ['auth']], function ()
 {
-    Route::get('/', 'HomeController@index')->name('home');
-    Route::get('/home', 'HomeController@index');
     Route::get('/logout', 'HomeController@destroy')->name('logout');
 
-    Route::get('/farmlab/create', 'PracticeController@create')->name('create');
-    Route::post('/farmlab/create', 'PracticeController@store');
+    Route::get('/farmlab/create', 'PracticeController@create')->name('farmlab.create');
+    Route::post('/farmlab/create', 'PracticeController@store')->name('farmlab.store');
 
     Route::get('/practice/create/vet', 'VetController@create')->name('vet.create');
     Route::post('/vet', 'VetController@store')->name('vet.store');
@@ -20,7 +20,7 @@ Route::group(['middleware' => ['auth']], function ()
     Route::post('/file', 'FileController@store')->name('file.store');
 
     Route::get('/labresults/index', 'LabResultController@index')->name('labresults.index')->middleware('practice');
-    Route::get('/labresults/farmer/{farmer}', 'LabResultController@index')->name('labresults.farmer');
+    Route::get('/labresults/farmer/{farmerName}', 'LabResultController@index')->name('labresults.farmer');
     Route::get('/labresults/{result}', 'LabResultController@show')->name('labresults.show');
     Route::post('/labresults/{result}', 'LabResultController@update')->name('labresults.process');
 
