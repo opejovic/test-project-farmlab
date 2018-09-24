0bd63ee6ba45396fe5e6eb947e614285fc53eda7
Ognjen Pejovic <opejovic@gmail.com>
2018-09-24 20:29:30 +0200

    Add models factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,11 +13,39 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'type' => App\Models\User::VET,
+        'status' => 'NOT_VERIFIED',
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\Practice::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word
+    ];
+});
+
+$factory->define(App\Models\LabResult::class, function (Faker $faker) {
+    return [
+        'herd_number' => $faker->randomNumber,
+        'date_of_arrival' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'date_of_test' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'animal_id' => $faker->numberBetween(1, 1000),
+        'lab_code' => $faker->randomNumber,
+        'test_name' => $faker->word,
+        'type_of_samples' => $faker->word,
+        'reading' => $faker->word,
+        'interpretation' => $faker->sentence,
+        'farmer_name' => $faker->name,
+        'vet_comment' => $faker->sentence,
+        'vet_indicator' => $faker->sentence,
+        'practice_id' => $faker->numberBetween(1,20),
+        'practice_name' => $faker->word
+    ];
+});
+
