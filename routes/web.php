450ca5c6931dcd829e055b54cf93cd165c350f4f
Ognjen Pejovic <opejovic@gmail.com>
2018-10-01 18:19:28 +0200

    Refactor routes middleware - added routes to middleware groups

--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,31 @@ Route::get('/logout', 'Auth\LoginController@getLogout');
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index');
 
-Route::group(['middleware' => ['auth']], function ()
+Route::middleware(['auth'])->group(function ()
 {
-    Route::get('/members/create', 'LabMemberController@create')->name('members.create')->middleware('farmlab.admin');
-    Route::post('/members/create', 'LabMemberController@store')->name('members.store')->middleware('farmlab.admin');
+    Route::middleware(['farmlab.admin'])->group(function () {
+        Route::get('/members/create',                 'LabMemberController@create')->name('members.create');
+        Route::post('/members/create',                'LabMemberController@store')->name('members.store');
+    });
+    
+    Route::middleware(['farmlab'])->group(function () {
+        Route::get('/practice/create',                'PracticeController@create')->name('practice.create');
+        Route::post('/practice/create',               'PracticeController@store')->name('practice.store');
+        Route::get('/file/upload',                    'FileController@create')->name('file.create');
+        Route::post('/file/upload',                   'FileController@store')->name('file.store');
+    });
 
-    Route::get('/practice/create', 'PracticeController@create')->name('practice.create')->middleware('farmlab');
-    Route::post('/practice/create', 'PracticeController@store')->name('practice.store')->middleware('farmlab');
+    Route::middleware(['practice.admin'])->group(function () {
+        Route::get('/vets',                           'VetController@index')->name('vet.index');
+        Route::get('/vets/create',                    'VetController@create')->name('vet.create');
+        Route::post('/vets',                          'VetController@store')->name('vet.store');
+        Route::get('/vets/{vet}',                     'VetController@show')->name('vet.show');
+    });
 
-    Route::get('/file/upload', 'FileController@create')->name('file.create')->middleware('farmlab');
-    Route::post('/file/upload', 'FileController@store')->name('file.store')->middleware('farmlab');
-
-    Route::get('/vets', 'VetController@index')->name('vet.index')->middleware('practice.admin');
-    Route::get('/vets/create', 'VetController@create')->name('vet.create')->middleware('practice.admin');
-    Route::post('/vets', 'VetController@store')->name('vet.store')->middleware('practice.admin');
-    Route::get('/vets/{vet}', 'VetController@show')->name('vet.show')->middleware('practice.admin');
-
-    Route::get('/labresults', 'LabResultController@index')->name('labresults.index')->middleware('practice');
-    Route::get('/labresults/farmer/{farmerName}', 'LabResultController@index')->name('labresults.farmer')->middleware('practice');
-    Route::get('/labresults/{result}', 'LabResultController@show')->name('labresults.show')->middleware('practice');
-    Route::post('/labresults/{result}', 'LabResultController@update')->name('labresults.process')->middleware('practice');
+    Route::middleware(['practice'])->group(function () {
+        Route::get('/labresults',                     'LabResultController@index')->name('labresults.index');
+        Route::get('/labresults/farmer/{farmerName}', 'LabResultController@index')->name('labresults.farmer');
+        Route::get('/labresults/{result}',            'LabResultController@show')->name('labresults.show');
+        Route::post('/labresults/{result}',           'LabResultController@update')->name('labresults.process');
+    });
 });
