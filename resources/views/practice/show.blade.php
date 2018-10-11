861836661c7284d5797f7fd23137373f351b321c
Ognjen Pejovic <opejovic@gmail.com>
2018-10-11 23:50:09 +0200

    practice.show view now displays practice members

--- resources/views/practice/show.blade.php
+++ resources/views/practice/show.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+        <div class="row">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body" id="card-div">
                         <table class="table text-center">
@@ -11,12 +11,10 @@
                                 <tr>
                                     <th class="card-header text-center border-top-0"><h4>Id</h4></th>
                                     <th class="card-header text-center border-top-0"><h4>Practice name</h4></th>
-                                    <th class="card-header text-center border-top-0"><h4>Created at</h4></th>
                                 </tr>
                                 <tr>
                                     <th class="text-capitalize border-bottom-1">{{ $practice->id }}</th>
                                     <th class="text-capitalize border-bottom-1">{{ $practice->name }}</th>
-                                    <th class="text-capitalize border-bottom-1">{{ $practice->created_at }}</th>
                                 </tr>
 
                             </tbody>
@@ -24,6 +22,31 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body" id="card-div">
+                        <table class="table text-center">
+                            <tbody>
+                                    <tr>
+                                        <th class="card-header text-center border-top-0"><h4>Id</h4></th>
+                                        <th class="card-header text-center border-top-0"><h4>Name</h4></th>
+                                        <th class="card-header text-center border-top-0"><h4>Role</h4></th>
+                                    </tr>
+                                @foreach ($practice->vets as $vet)
+                                    <tr>
+                                        <th class="text-capitalize border-bottom-1">{{ $vet->id }}</th>
+                                        <th class="text-capitalize border-bottom-1">{{ $vet->name }}</th>
+                                        <th class="text-capitalize border-bottom-1">{{ $vet->type }}</th>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>                              
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
