a4d41f5b30b3f1bfb802617f036b3a81897b1518
Ognjen Pejovic <opejovic@gmail.com>
2018-11-14 20:43:49 +0100

    refactor error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,39 +1,18 @@
-<!DOCTYPE html>
-<html>
+@extends ('errors.master')
 
-<head>
+@section ('title', '404 Error')
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@section ('content')
+    <h1>404</h1>
+    <h3 class="font-bold">Page Not Found</h3>
 
-    <title>INSPINIA | 404 Error</title>
-
-    <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}" />
-    <link rel="stylesheet" href="{!! asset('css/app.css') !!}" />
-
-</head>
-
-<body class="gray-bg">
-
-
-    <div class="middle-box text-center animated fadeInDown">
-        <h1>404</h1>
-        <h3 class="font-bold">Page Not Found</h3>
-
-        <div class="error-desc">
-            Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the refresh button on your browser or try found something else in our app.
-            <form class="form-inline m-t" role="form">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search for page">
-                </div>
-                <button type="submit" class="btn btn-primary">Search</button>
-            </form>
-        </div>
+    <div class="error-desc">
+        Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the refresh button on your browser or try found something else in our app.
+        <form class="form-inline m-t" role="form">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search for page">
+            </div>
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
     </div>
-
-    <!-- Scripts -->
-    <script src="{!! asset('js/app.js') !!}"></script>
-
-</body>
-
-</html>
+@endsection
