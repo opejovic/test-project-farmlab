3847c77d67e00eea3ab95f0972ced091701ff7de
Ognjen Pejovic <opejovic@gmail.com>
2018-11-14 19:49:29 +0100

    delete unused blade files

--- resources/views/layouts/archives.blade.php
+++
@@ -1,9 +0,0 @@
- <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Browse lab results
-    </a>
-    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-      <a class="dropdown-item" href="{{ route('labresults.index') }}">All results</a>
-      <a class="dropdown-item" href="/labresults?my">My results</a> 
-    </div>
-</li>
