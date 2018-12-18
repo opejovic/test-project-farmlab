76e752aea0379c6d02933b1f2cf1dff8dba6f7bf
Ognjen Pejovic <opejovic@gmail.com>
2018-12-18 14:06:57 +0100

    work in progress

---
+++ database/migrations/2018_12_18_125611_add_processed_at_to_labresults_table.php
@@ -0,0 +1,33 @@
+            $table->timestamp('email_verified_at')->nullable();
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProcessedAtToLabresultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('labresults', function (Blueprint $table) {
+            $table->timestamp('processed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('labresults', function (Blueprint $table) {
+            //
+        });
+    }
+}
